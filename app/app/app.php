eb8905ad521dd9979410385ee9e3532ec55e06fe
vladz <vlad.zinko@mindgeekconsultants.com>
2021-01-27 17:34:24 +0200

    initical commit

---
+++ app/app/app.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Local variables
+ * @var \Phalcon\Mvc\Micro $app
+ */
+
+/**
+ * Add your routes here
+ */
+$app->get('/', function () {
+    echo $this['view']->render('index');
+});
+
+$app->get(
+    '/api/{id:[0-9]+}',
+    function ($id) {
+        
+    }
+);
+
+/**
+ * Not found handler
+ */
+$app->notFound(function () use($app) {
+    $app->response->setStatusCode(404, "Not Found")->sendHeaders();
+    echo $app['view']->render('404');
+});
